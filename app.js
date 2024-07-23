4694a1eb6f804aa5c79f1218564c256ca1b0f08e
Felix Davis-Eldridge <daviseldridgef@papanui.school.nz>
2024-07-24 11:06:30 +1200

    Seconf Commit

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+// Author: Felix Davis-Eldridge
+// Date created: 17/06/2024
+// Last updated: 24/06/2024
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classlist.add('show'); //determines whether elements on the page will be visible or not
-        } else { 
-            entry.target.classlist.remove('show');
-        }
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show'); // determines whether elements on a page are visible or not
+      } else { 
+        entry.target.classList.remove('show');
+      }
     });
-});
-        
-const hiddenElements = document.querySelectorAll('.hidden'); 
-hiddenElements.forEach((el) => observer.observe(el)); 
-/*
-grabbing all the elements under the hidden class 
-so they can have an animation through CSS styling applied later on 
-*/
-
+  }, {}); 
+  
+  const hiddenElements = document.querySelectorAll('.hidden'); 
+  hiddenElements.forEach((el) => observer.observe(el)); // grabs all hidden elements
